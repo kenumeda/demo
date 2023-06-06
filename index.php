5742c41b7a72cd280caf3c19a6885da26d0095b3
Ken Umeda <kenumeda1@gmail.com>
2023-06-06 22:39:12 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+    $mode = "dark"; 
+    $name = "Ken";
+
+
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Demo</title>
+</head>
+<body>
+
+<?php 
+echo "Hello {$name}!" ?>
+
+    
+</body>
+</html>
